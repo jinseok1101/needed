f9a0a65fffa6682e6796bf361556c1d09791f117
jinseok <riaudfgneenh@gmail.com>
2022-05-20 21:03:42 +0900

    Update Random PW generator.py

--- Simple things/Random PW generator.py
+++ Simple things/Random PW generator.py
@@ -1,3 +1,5 @@
+#2022 / 05 / 18
+
 #몇개나 만들지, 얼마나 길게 만들지를 치면 영어 소문자 + 대문자 + 특수기호로 양자컴퓨터 아니면 푸는데 오래걸릴 비번 만들어줌
 #It creates a difficult password. It would be safe if hackers didn't use quantum computers
 
