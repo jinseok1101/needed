a001e9609f5243ca41ebddfac62b048d879d99fd
jinseok <riaudfgneenh@gmail.com>
2022-06-01 00:58:12 +0900

    Delete Preparing for studying.py

--- Simple things/Preparing for studying.py
+++
@@ -1,8 +0,0 @@
-#2022 / 05 / 21
-
-#코딩 공부 할 때 일단 열어두는 것들
-
-import webbrowser
-a = input('study start = start : ')
-if a == 'start':
-    webbrowser.open("https://codeup.kr/problemsetsol.php?psid=33") and webbrowser.open("https://www.acmicpc.net/step") and webbrowser.open("https://github.com/jinseok1101") and webbrowser.open("https://jinseok1101.tistory.com") and webbrowser.open("https://google.com") 
