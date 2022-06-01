95306e14f0c12f0280a2437e70ead39b91d26e4a
jinseok <riaudfgneenh@gmail.com>
2022-06-01 23:01:10 +0900

    Create (미완) 평균 업글.py

---
+++ Simple things/(미완) 평균 업글.py
@@ -0,0 +1,14 @@
+#2022 / 5 / 30 ~ 미완
+#평균 구하는건데 총 몇갠지 모를 상황에서 쓸 수 있게 업글한거
+
+b = '0'
+while True:
+	if 'end' in b:
+		b.remove('end')
+		c = float(b)
+		print(sum(c)/len(c))
+		break
+	else:
+		b = [input('ghgyguygy : ')]
+		
+
