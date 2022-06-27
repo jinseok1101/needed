ddb761244a563dc4eb69fccc7bdfc5acf7d15257
jinseok <riaudfgneenh@gmail.com>
2022-06-28 06:58:37 +0900

    Update 이게 소순가?.py

--- 이게 소순가?.py
+++ 이게 소순가?.py
@@ -2,13 +2,13 @@
 
 # 알고싶은 숫자를 넣으면 소수인지 판단해줌. 근데 속도가 좀 많이 느림. 숫자 좀 크게하면 거의 벤치마크 대용으로 써도 될듯ㅋㅋ
 
-a = int(input('Enter the number you want to know : '))
+a = int(input('알고 싶은 숫자를 넣어주세요. : '))
 b=0
 for i in range(2,a):
 	if a%i==0:
 		b=1
 		
 if b==0:
-	print('%s is prime number' % a)
+	print('%s 는 소수입니다' % a)
 else:
-	print('%s is not prime number' % a)
+	print('%s 는 소수가 아닙니다' % a)
