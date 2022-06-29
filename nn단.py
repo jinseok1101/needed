13e2c575e735ae71ad89b6b579386adfc6984a43
jinseok <riaudfgneenh@gmail.com>
2022-06-29 21:35:27 +0900

    Update nn단.py

--- nn단.py
+++ nn단.py
@@ -4,8 +4,8 @@
 # 몇단 쓸지 넣고, 얼마나 길게 구하고 싶은지 쓰면 해줌
 # 와! 222222같은 숫자들을 1부터 원하는 수까지 구한 값을 다 볼 수 있다고? 정말 굉장하다! 
 
-n = int(input('Times table : '))
-c = int(input('How long? : '))
+n = int(input('몇단? : '))
+c = int(input('얼마나 길게? : '))
 print("""
 
 ⸻⸻""")
